6e269de21f801e735905894147f2f9455bd31846
ketan2411 <ketansharma587@gmail.com>
2020-05-06 16:16:29 +0530

    base layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <v-app id="inspire">
+    <!--  navigation-drawer work on desktop res:1080p -->
     <v-navigation-drawer
-      v-model="drawerRight"
+      v-model="drawer"
       app
-      clipped
-      right
     >
       <v-list dense>
-        <v-list-item @click.stop="right = !right">
+        <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-exit-to-app</v-icon>
+            <v-icon>mdi-home</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -20,96 +27,45 @@
 
     <v-app-bar
       app
-      clipped-right
-      color="blue-grey"
+      color="indigo"
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Toolbar</v-toolbar-title>
-      <v-spacer />
-      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight" />
+      <v-toolbar-title>Docker</v-toolbar-title>
+      <v-spacer></v-spacer>
+        <div class="text-center">
+            <v-menu open-on-hover top offset-y>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  color="primary"
+                  dark
+                  v-on="on"
+                >
+                  Dropdown
+                </v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  @click=""
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-item @click.stop="left = !left">
-          <v-list-item-action>
-            <v-icon>mdi-exit-to-app</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-navigation-drawer
-      v-model="left"
-      fixed
-      temporary
-    />
-
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          justify="center"
-          align="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/QewYYx"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
-      </v-container>
+    <router-view></router-view>
     </v-content>
-
-    <v-navigation-drawer
-      v-model="right"
-      fixed
-      right
-      temporary
-    />
-
     <v-footer
+      color="indigo"
       app
-      color="blue-grey"
-      class="white--text"
     >
-      <span>Vuetify</span>
-      <v-spacer />
-      <span>&copy; 2019</span>
+      <span class="white--text">&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
@@ -121,9 +77,12 @@
     },
     data: () => ({
       drawer: null,
-      drawerRight: null,
-      right: false,
-      left: false,
+       items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ]
     }),
   }
 </script>
